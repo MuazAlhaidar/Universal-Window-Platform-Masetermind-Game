53ef282c0755365fc05004dc6e335b6dce9cbf95
MuazAlhaidar <omunam10@gmail.com>
2020-04-20 15:54:28 -0400

    Added a title, start button, credits button, and instructions button to the main menu
    When the buttons are clicked then they will output to the Output (Debug mode) tab if the click event worked

--- MasterMind-UWP-Edition/MasterMind-UWP-Edition/MainPage.xaml.cs
+++ MasterMind-UWP-Edition/MasterMind-UWP-Edition/MainPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Microsoft.Graphics.Canvas.Text;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Diagnostics;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -13,6 +16,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -31,6 +35,11 @@ namespace MasterMind_UWP_Edition
         private void Canvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.ICanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedDrawEventArgs args) {
 
             args.DrawingSession.DrawRectangle(10, 10, 500, 700, Colors.DeepSkyBlue);
+
+            DrawGameTitle(args);
+            DrawStartButton(args);
+            DrawInstructionsButton(args);
+            DrawCreditsButton(args);
         }
 
         private void Canvas_Update(Microsoft.Graphics.Canvas.UI.Xaml.ICanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedUpdateEventArgs args) {
@@ -41,6 +50,90 @@ namespace MasterMind_UWP_Edition
         private void Canvas_Create_Resources(Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.CanvasCreateResourcesEventArgs args) {
 
 
+        }
+
+        private static void DrawGameTitle(CanvasAnimatedDrawEventArgs args) {
+
+            using (CanvasTextFormat format = new CanvasTextFormat {
+
+                HorizontalAlignment = CanvasHorizontalAlignment.Center,
+                VerticalAlignment = CanvasVerticalAlignment.Center,
+
+                WordWrapping = CanvasWordWrapping.NoWrap,
+
+                FontSize = 50.0f
+            }) {
+
+                args.DrawingSession.DrawText("Master Mind", 250, 150, Colors.DeepPink, format);
+            }
+        }
+
+        private static void DrawStartButton(CanvasAnimatedDrawEventArgs args) {
+
+            using (CanvasTextFormat format = new CanvasTextFormat {
+
+                HorizontalAlignment = CanvasHorizontalAlignment.Center,
+                VerticalAlignment = CanvasVerticalAlignment.Center,
+
+                WordWrapping = CanvasWordWrapping.NoWrap,
+
+                FontSize = 30.0f
+            }) {
+
+                args.DrawingSession.DrawText("Start", 250, 300, Colors.DeepSkyBlue, format);
+                args.DrawingSession.DrawRectangle(150, 275, 200, 55, Colors.DeepPink);
+            }
+        }
+
+        private static void DrawInstructionsButton(CanvasAnimatedDrawEventArgs args) {
+
+            using (CanvasTextFormat format = new CanvasTextFormat {
+
+                HorizontalAlignment = CanvasHorizontalAlignment.Center,
+                VerticalAlignment = CanvasVerticalAlignment.Center,
+
+                WordWrapping = CanvasWordWrapping.NoWrap,
+
+                FontSize = 30.0f
+            }) {
+
+                args.DrawingSession.DrawText("Instructions", 250, 400, Colors.DeepSkyBlue, format);
+                args.DrawingSession.DrawRectangle(150, 375, 200, 55, Colors.DeepPink);
+            }
+        }
+
+        private static void DrawCreditsButton(CanvasAnimatedDrawEventArgs args) {
+
+            using (CanvasTextFormat format = new CanvasTextFormat {
+
+                HorizontalAlignment = CanvasHorizontalAlignment.Center,
+                VerticalAlignment = CanvasVerticalAlignment.Center,
+
+                WordWrapping = CanvasWordWrapping.NoWrap,
+
+                FontSize = 30.0f
+            }) {
+
+                args.DrawingSession.DrawText("Credits", 250, 500, Colors.DeepSkyBlue, format);
+                args.DrawingSession.DrawRectangle(150, 475, 200, 55, Colors.DeepPink);
+            }
+        }
+
+        // Lines are written out in the Output Tab When Debugging (so that linux users can debug)
+        // Currently all buttons clicks do is print out to the Output (Debug Setting) window when they where pressed
+        private void StartButton_Click(object sender, RoutedEventArgs e) {
+
+            Debug.WriteLine("(Start) Click Event");
+        }
+
+        private  void InstructionsButton_Click(object sender, RoutedEventArgs e) {
+
+            Debug.WriteLine("(Instructions) Click Event");
+        }
+
+        private void CreditsButton_Click(object sender, RoutedEventArgs e) {
+
+            Debug.WriteLine("(Credits) Click Event");
         }
     }
 }
