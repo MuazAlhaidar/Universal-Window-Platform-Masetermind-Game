a6613e0b2511fcca5f988bb45745f995be4688f8
John <spacecowboy2397@gmail.com>
2020-04-23 20:39:10 -0400

    Update CreditsPage.xaml.cs

--- MasterMind-UWP-Edition/MasterMind-UWP-Edition/CreditsPage.xaml.cs
+++ MasterMind-UWP-Edition/MasterMind-UWP-Edition/CreditsPage.xaml.cs
@@ -1,44 +1,127 @@
-﻿using System;
+﻿using Microsoft.Graphics.Canvas;
+using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Reflection.Context;
+using System.Text;
+using System.Threading.Tasks;
 using Windows.UI;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+namespace MasterMind_UWP_Edition
+{
 
-namespace MasterMind_UWP_Edition {
+    public class Mastermind
+    {
 
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class CreditsPage : Page {
+        public CanvasDrawingSession session;
 
-        public CreditsPage() {
+        public int CurrentRow { get; set; }
 
-            this.InitializeComponent();
+        public List<Color> PegColors = new List<Color> { Colors.Red, Colors.Blue, Colors.Green, Colors.Yellow, Colors.Purple, Colors.Cyan };
+        public List<Color> HintColors = new List<Color> { Colors.Green, Colors.Yellow, Colors.SlateGray };
+
+        public List<List<Peg>> Pegs;
+        public List<List<Peg>> HintPegs;
+        public List<Peg> PegSecretCode;
+
+        public Mastermind()
+        {
+
+            CurrentRow = 0;
+
+            Pegs = new List<List<Peg>>();
+
+            int XCoordinate;
+            int YCoordinate = 50;
+            int newRadius = 20;
+
+            for (int rowNumber = 0; rowNumber < 10; rowNumber++)
+            {
+
+                var row = new List<Peg>();
+
+                XCoordinate = 50;
+
+                for (int column = 0; column < 4; column++)
+                {
+
+                    var peg = new Peg()
+                    {
+
+                        X = XCoordinate,
+                        Y = YCoordinate,
+                        Radius = newRadius,
+                        Color = Colors.SlateGray
+                    };
+
+                    row.Add(peg);
+
+                    XCoordinate += 60;
+                }
+
+                YCoordinate += 50;
+                Pegs.Add(row);
+            }
         }
 
-        private void Canvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.ICanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedDrawEventArgs args) {
+        public Color NextColor(Color currentColor)
+        {
 
-            args.DrawingSession.DrawRectangle(10, 10, 500, 700, Colors.DeepSkyBlue);
+            if (currentColor == Colors.SlateGray)
+            {
+
+                return PegColors[0];
+            }
+            else if (currentColor == Colors.Red)
+            {
+
+                return PegColors[1];
+            }
+            else if (currentColor == Colors.Blue)
+            {
+
+                return PegColors[2];
+            }
+            else if (currentColor == Colors.Green)
+            {
+
+                return PegColors[3];
+            }
+            else if (currentColor == Colors.Yellow)
+            {
+
+                return PegColors[4];
+            }
+            else if (currentColor == Colors.Purple)
+            {
+
+                return PegColors[5];
+            }
+            else if (currentColor == Colors.Cyan)
+            {
+
+                return PegColors[0];
+            }
+
+            return default;
         }
 
-        private void Canvas_Update(Microsoft.Graphics.Canvas.UI.Xaml.ICanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedUpdateEventArgs args) {
+        public void DrawMastermind(CanvasDrawingSession drawingSession)
+        {
 
+            for (int rowIndex = 0; rowIndex < Pegs.Count; rowIndex++)
+            {
 
+                for (int columnIndex = 0; columnIndex < Pegs[rowIndex].Count; columnIndex++)
+                {
+
+                    Pegs[rowIndex][columnIndex].Draw(drawingSession);
+                }
+            }
         }
 
-        private void Canvas_Create_Resources(Microsoft.Graphics.Canvas.UI.Xaml.CanvasAnimatedControl sender, Microsoft.Graphics.Canvas.UI.CanvasCreateResourcesEventArgs args) {
+        public void CheckIfCorrect()
+        {
 
 
         }
